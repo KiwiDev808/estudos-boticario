3906de81c7aea9ab22fee8dd0758d6c9f73dc6b0
KiwiDev808 <lucasbacela@hotmail.com>
2021-08-10 17:32:55 -0300

    feat: add ts-node-dev and todo basic CRUD

--- padroes-rest/src/controller/todo.ts
+++ padroes-rest/src/controller/todo.ts
@@ -1,9 +1,47 @@
 import { Request, Response, Router } from 'express'
+import Todo from '../models/todo'
 
 const todoRouter = Router()
 
-todoRouter.get('/', (req: Request, res: Response) => {
-  return res.send({ message: 'test' })
+todoRouter.get('/', async (req: Request, res: Response) => {
+  const todoList = await Todo.find({})
+  return res.send({ todoList })
+})
+
+todoRouter.post('/', async (request, response) => {
+  const body = request.body
+
+  if (!body?.author && !body?.description) {
+    return response.status(400).json({
+      error: 'author or description is missing',
+    })
+  }
+
+  const todo = new Todo({
+    ...body,
+    status: 'todo',
+  })
+
+  const result = await todo.save()
+  response.status(201).json(result)
+})
+
+todoRouter.put('/:id', async (request, response) => {
+  const body = request.body
+
+  const todo = {
+    status: body.status,
+  }
+
+  const updatedTodo = await Todo.findByIdAndUpdate(request.params.id, todo, {
+    new: true,
+  })
+  response.json(updatedTodo.toJSON())
+})
+
+todoRouter.delete('/:id', async (request, response) => {
+  await Todo.findByIdAndDelete(request.params.id)
+  response.status(204).end()
 })
 
 export { todoRouter }
