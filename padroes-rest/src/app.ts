99bf5fe767c121433ce3162e89bcf81207e2b373
KiwiDev808 <lucasbacela@hotmail.com>
2021-08-10 16:40:48 -0300

    feat: add initial dependencies and express test route

---
+++ padroes-rest/src/app.ts
@@ -0,0 +1,11 @@
+import cors from 'cors'
+import express, { Request, Response } from 'express'
+
+const app = express()
+app.use(express.json())
+app.use(cors())
+
+app.get('/ping', (req: Request, res: Response) => {
+  return res.send({ message: 'pong' })
+})
+export { app }
