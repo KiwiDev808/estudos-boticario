0828d8fdf486a6bb39b9effd5d4fa6e4162a4d6c
KiwiDev808 <lucasbacela@hotmail.com>
2021-08-12 19:26:52 -0300

    test(test): add validString function test cases

--- padroes-rest/test/unit/validString.spec.ts
+++ padroes-rest/test/unit/validString.spec.ts
@@ -1,3 +1,27 @@
+import { validString } from '../../src/utils/validString'
+
 describe('Valid string', () => {
-  it('should return false to empty values', () => {})
+  it('should return true to valid string values', () => {
+    expect(validString('hello world')).toBe(true)
+  })
+  it('should return false to empty values', () => {
+    expect(validString('')).toBe(false)
+  })
+  describe('should return false to non string values', () => {
+    it('should return false to number', () => {
+      expect(validString(123)).toBe(false)
+    })
+    it('should return false to array', () => {
+      expect(validString([])).toBe(false)
+    })
+    it('should return false to object', () => {
+      expect(validString({})).toBe(false)
+    })
+    it('should return false to null', () => {
+      expect(validString(null)).toBe(false)
+    })
+    it('should return false to boolean', () => {
+      expect(validString(true)).toBe(false)
+    })
+  })
 })
