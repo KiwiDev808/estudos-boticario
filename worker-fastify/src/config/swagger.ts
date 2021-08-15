e3a15ce515d139b956a4b0fc948c03078be60ec9
KiwiDev808 <lucasbacela@hotmail.com>
2021-08-15 12:10:49 -0300

    refactor(worker-fastify): move swagger options to it own file

---
+++ worker-fastify/src/config/swagger.ts
@@ -0,0 +1,24 @@
+import { SwaggerOptions } from 'fastify-swagger'
+
+const swaggerOptions: SwaggerOptions = {
+  routePrefix: '/doc',
+  swagger: {
+    info: {
+      title: 'Test swagger',
+      description: 'Testing the Fastify swagger API',
+      version: '0.1.0',
+    },
+    externalDocs: {
+      url: 'https://swagger.io',
+      description: 'Find more info here',
+    },
+    host: 'localhost:3003',
+    schemes: ['http'],
+    consumes: ['application/json'],
+    produces: ['application/json'],
+  },
+
+  exposeRoute: true,
+}
+
+export { swaggerOptions }
--- worker-fastify/src/index.ts
+++ worker-fastify/src/index.ts
@@ -1,29 +1,12 @@
 import fastify from 'fastify'
+import fastifySwagger from 'fastify-swagger'
+import { swaggerOptions } from './config/swagger'
 
 const server = fastify({
   logger: true,
 })
 
-server.register(require('fastify-swagger'), {
-  routePrefix: '/doc',
-  swagger: {
-    info: {
-      title: 'Test swagger',
-      description: 'Testing the Fastify swagger API',
-      version: '0.1.0',
-    },
-    externalDocs: {
-      url: 'https://swagger.io',
-      description: 'Find more info here',
-    },
-    host: 'localhost:3003',
-    schemes: ['http'],
-    consumes: ['application/json'],
-    produces: ['application/json'],
-  },
-
-  exposeRoute: true,
-})
+server.register(fastifySwagger, swaggerOptions)
 
 server.get('/ping', async (request, reply) => {
   return reply.send({ message: 'pong\n' })
